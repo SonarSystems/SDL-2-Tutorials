48e7d3489deed5b882be7ee18f3d87f8fac8e371
Sonar Systems <sonarsystems@frahaans-imac.home>
2017-01-09 09:07:48 +0000

    Update Tutorial 1 Mac Xcode Code

--- Tutorial 001_Setup/Mac/SDL Tutorial/main.cpp
+++ Tutorial 001_Setup/Mac/SDL Tutorial/main.cpp
@@ -4,9 +4,9 @@
 
 int main( int argc, char *argv[] )
 {
-    if( SDL_Init( SDL_INIT_EVERYTHING ) < 0 )
+    if ( SDL_Init( SDL_INIT_EVERYTHING ) < 0 )
     {
-        std::cout << "SDL could not initialize! SDL_Error: %s\n" << SDL_GetError() << std::endl;
+        std::cout << "SDL could not initialize! SDL Error: " << SDL_GetError( ) << std::endl;
     }
     
     return EXIT_SUCCESS;
